12fb145aa1167378895d8d587c0cdf4e4df131da
Trisha Nandi <nandi.trisha@gmail.com>
2025-11-09 20:35:33 +0530

    Remove viewdidappear, add viewwillappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         //
     }
 
